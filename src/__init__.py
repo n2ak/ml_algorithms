93e4da5213312c8a3704f980f6f7d1ad5e84221c
n2ak <n2akajdaou@gmail.com>
2023-04-17 18:03:16 +0000

    added Module class

--- src/__init__.py
+++ src/__init__.py
@@ -4,3 +4,27 @@ from .layers import *
 from .tensor import *
 from .utils import *
 from .loss import *
+from .optimizer import *
+
+
+class Module(HasForwardAndIsCallable):
+    # TODO
+    def __init__(self) -> None:
+        self.params = None
+
+    def zero_grad(self):
+        params = self.get_parameters()
+        for p in params:
+            p.zero_grad()
+
+    def get_parameters(self) -> list[Tensor]:
+        if self.params:
+            return self.params
+        import inspect
+        p = []
+        for k, v in inspect.getmembers(self):
+            if isinstance(v, Trainable):
+                v: Trainable = v
+                p.extend(v.get_trainable_params())
+        self.params = p
+        return self.params
