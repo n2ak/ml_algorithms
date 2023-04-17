f52da6cc07a05059f548023b58cc278c8389ff80
n2ak <n2akajdaou@gmail.com>
2023-04-17 18:05:26 +0000

    improved api

--- src/tensor.py
+++ src/tensor.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 from typing import List, Callable
 import numpy as np
-from .grad import AddGradFn, DivGradFn, ExpGradFn, GradFn, IdentityGradFn, MatMulGradFn, MeanGradFn, MulGradFn, PowGradFn, SubGradFn, SumGradFn
+from .grad import LogGradFn, AddGradFn, DivGradFn, ExpGradFn, GradFn, IdentityGradFn, MatMulGradFn, MeanGradFn, MulGradFn, PowGradFn, SubGradFn, SumGradFn
 
-from .utils import biased, cross_entropy, is_scalar, linear, log_softmax, negative_log_likelihood, relu, sequential, sigmoid, softmax
+from .activation import log_softmax, relu,  sigmoid, softmax
+from .utils import biased, cross_entropy, is_scalar, linear, negative_log_likelihood, sequential
 
 
 class Tensor(np.ndarray):
@@ -27,6 +28,12 @@ class Tensor(np.ndarray):
 
     def set_grad(self, grad):
         self._grad = grad
+        assert tuple(self.shape) == tuple(
+            self._grad.shape), f"{tuple(self.shape)},{tuple(self._grad.shape)}"
+
+    def zero_grad(self):
+        # self._grad *=0
+        self._grad = np.zeros_like(self._grad)
 
     def _init(self, requires_grad):
         self._is_leaf = True
@@ -40,16 +47,28 @@ class Tensor(np.ndarray):
         return arr
 
     @staticmethod
-    def zeros_like(tensor):
-        return Tensor.array(np.zeros_like(tensor))
+    def ns_like(x, n):
+        return Tensor.ns(x.shape, n)
 
     @staticmethod
-    def ns_like(tensor, n):
-        return Tensor.zeros_like(tensor) + n
+    def zeros_like(x):
+        return Tensor.ns_like(x.shape, 0)
 
     @staticmethod
-    def ones_like(tensor):
-        return Tensor.ns_like(tensor, 1)
+    def ones_like(x):
+        return Tensor.ns_like(x.shape, 1)
+
+    @staticmethod
+    def ns(shape, k):
+        return tensor(np.zeros(shape)+k)
+
+    @staticmethod
+    def ones(shape):
+        return Tensor.ns(shape, 1)
+
+    @staticmethod
+    def zeros(shape):
+        return Tensor.ns(shape, 0)
 
     def is_a_leaf(self): return self._is_leaf
 
@@ -69,7 +88,7 @@ class Tensor(np.ndarray):
 
     def backward(self, gradient=1):
         assert self.can_calculatebackward(gradient)
-        self.grad_fn.calculate()
+        self.grad_fn.calculate(gradient)
 
     def is_scalar(self):
         # return np.isscalar(self)
@@ -87,13 +106,26 @@ class Tensor(np.ndarray):
                 return True
         return False
 
+    @classmethod
+    def check_requires_grad(cls, t: Tensor, other=None):
+        return t.requires_grad or (
+            other.requires_grad if isinstance(other, Tensor) else False)
+
     def bin_op(self, other, func, grad_fn, *args, **kwargs):
-        a = Tensor.array(func(self, other, *args, **kwargs))
-        return a.check_required(grad_fn, [self, other])
+        requires_grad = Tensor.check_requires_grad(self, other)
+        a = tensor(func(self, other, *args, **kwargs),
+                   requires_grad=requires_grad)
+        if a.requires_grad:
+            a.from_op(grad_fn([self, other]))
+        return a
 
     def op(self, func, grad_fn, *args, **kwargs):
-        a = Tensor.array(func(self, *args, **kwargs))
-        return a.check_required(grad_fn, [self])
+        requires_grad = Tensor.check_requires_grad(self, None)
+        a = tensor(func(self, *args, **kwargs),
+                   requires_grad=requires_grad)
+        if a.requires_grad:
+            a.from_op(grad_fn([self]))
+        return a
 
     def __add__(self, other): return self.bin_op(other, np.add, AddGradFn)
     def __sub__(self, other): return self.bin_op(other, np.subtract, SubGradFn)
@@ -126,34 +158,36 @@ class Tensor(np.ndarray):
         import torch
         return torch.tensor(self.numpy(), requires_grad=self.requires_grad)
 
-    def mean(self, *args, **kwargs):
-        r = Tensor.array(self.numpy().mean(*args, **kwargs))
-        return r.check_required(MeanGradFn, [self])
+    def _extend_numpy(self, a, grad_fn, axis):
+        r = tensor(a, requires_grad=self.requires_grad)
+        if r.requires_grad:
+            r.from_op(grad_fn([self], axis))
+        return r
 
-    def sum(self, *args, **kwargs):
-        r = Tensor.array(self.numpy().sum(*args, **kwargs))
-        return r.check_required(SumGradFn, [self])
+    def mean(self, axis=None, keepdims=False):
+        a = self.numpy().mean(axis=axis, keepdims=keepdims)
+        return self._extend_numpy(a, MeanGradFn, axis=axis)
 
-    def check_required(self, grad_fn, dep):
-        requires_grad = Tensor.filter(dep)
-        if not requires_grad:
-            return self
-        self.from_op(grad_fn(dep))
-        return self
+    def sum(self, axis=None, keepdims=False):
+        a = self.numpy().sum(axis=axis, keepdims=keepdims)
+        return self._extend_numpy(a, SumGradFn, axis=axis)
 
     @classmethod
-    def rand(cls, *args): return Tensor.array(np.random.rand(*args))
+    def rand(cls, *args): return tensor(np.random.rand(*args))
     @classmethod
-    def zeros(cls, *args): return Tensor.array(np.zeros(*args))
+    def zeros(cls, *args): return tensor(np.zeros(*args))
 
     def __repr__(self) -> str:
-        v = (f".val          ={self.numpy()}",
-             f".requires_grad={self.requires_grad}",
-             f".grad_fn      ={self.grad_fn}")
-        return "<tensor.Tensor\n%s\n%s\n%s>" % v
+        v = (
+            # f".val          = {self.numpy()}",
+            f".requires_grad= {self.requires_grad}",
+            f".grad_fn      = {type(self.grad_fn).__name__}"
+        )
+        t = "\n".join(v)
+        return f'<nn.Tensor\n{t}/>'
 
     def __str__(self) -> str:
-        return self.__repr__()
+        return str(self.numpy())
 
     linear = linear
     relu = relu
@@ -167,8 +201,20 @@ class Tensor(np.ndarray):
 
     unique = lambda self, *args, **kwargs: np.unique(self, *args, **kwargs)
 
+    # def exp(self, *args, **kwargs) -> Tensor:
+    #     a = self.numpy()
+    #     print(np.array(a).dtype)
+    #     return np.exp(a)
     def exp(self, *args, **kwargs) -> Tensor: ...
+
     def log(self, *args, **kwargs) -> Tensor: ...
+
+    def check_required(self, grad_fn, dep):
+        requires_grad = Tensor.filter(dep)
+        if not requires_grad:
+            return self
+        self.from_op(grad_fn(dep))
+        return self
 
 
 def bind():
@@ -182,7 +228,7 @@ def bind():
         return setattr(cls, func.__name__, lambda self, *args, **kwargs: perform(self, *args, **kwargs))
     methods = [
         (np.exp, ExpGradFn),
-        (np.log, None),
+        (np.log, LogGradFn),
 
         # (np.unique,None),
 
@@ -202,3 +248,4 @@ def bind():
 
 
 bind()
+tensor = Tensor.array
